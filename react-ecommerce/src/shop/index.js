c248844bbe950cdb9a3095c999c0ecf063cc266f
Jan <jan@landert.cc>
2022-07-10 23:06:39 +0200

    /100

--- react-ecommerce/src/shop/index.js
+++ react-ecommerce/src/shop/index.js
@@ -4,14 +4,14 @@ const SHOP_DATA = [
         title: 'Air Pods Cleaner Premium',
         description: 'With the Air Pods Cleaner Premium you can clean your Air Pods so easily.',
         imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSooZ8fiPNcTeqobaSshVRZu31Tin0-HVK6A&usqp=CAU',
-        price: 1499,
+        price: 14.99,
     },
     {
         id: 2,
         title: 'Air Pods Cleaner Standard',
         description: 'With the Air Pods Cleaner Standard you can clean your Air Pods so easily.',
         imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXDI5snKph3KSxvWXK3qUfMBD-aW8nDtEhEg&usqp=CAUhttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXDI5snKph3KSxvWXK3qUfMBD-aW8nDtEhEg&usqp=CAU',
-        price: 999,
+        price: 9.99,
     }
 ];
 
