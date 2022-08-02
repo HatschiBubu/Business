412450ada131bc5fcf7c8393ecb0b8791affd50a
Jan <jan@landert.cc>
2022-08-02 22:56:40 +0200

    asdfsad

--- react-ecommerce/src/helpers.js
+++ react-ecommerce/src/helpers.js
@@ -2,7 +2,7 @@ export const isInCart = (product, cartItems) => {
     return cartItems.find(item => item.id === product.id);
 }
 
-const API = 'http://ec2-3-73-74-49.eu-central-1.compute.amazonaws.com:8080';
+const API = 'https://ec2-3-73-74-49.eu-central-1.compute.amazonaws.com:8080';
 
 export async function fetchFromAPI(endpoint, opts) {
     const { method, body } = { method: 'POST', body: null, ...opts }
