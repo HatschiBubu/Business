6c4f515ebd98a602b5d6ac8897d346fd06c44db7
Jan <jan@landert.cc>
2022-08-08 15:36:03 +0200

    s

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const corsOptions ={
 
 app.use(cors(corsOptions)) // Use this after the variable declaration
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.post('/create-checkout-session', createCheckoutSession);
 
