a1860d0c81d2fbb033129c174764d36020ce1802
Jan <jan@landert.cc>
2022-09-05 17:11:30 +0200

    fds

--- server/index.js
+++ server/index.js
@@ -6,25 +6,12 @@ const webhook = require('./api/webhook');
 
 const app = express();
 const port = 8080;
-const server = app.listen(port, () => console.log('server listening on port', port));
-
-server.keepAliveTimeout = 65000;
-server.headersTimeout = 70000;
 
 app.use(express.json({
     verify: (req, res, buffer) => req['rawBody'] = buffer,
 }));
 
-app.options('/', cors()) // enable pre-flight request for DELETE request
-app.options('/create-checkout-session', cors())
-
-app.delete('/', cors(), function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-
-app.delete('/create-checkout-session', cors(), function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-  })
+app.use(cors({ origin: true }));
 
 app.get('/', (req, res) => res.send('warum tuet me mir das ah3'));
 
@@ -32,3 +19,4 @@ app.post('/create-checkout-session', createCheckoutSession);
 
 app.post('/webhook', webhook);
 
+app.listen(port, () => console.log('server listening on port', port));
